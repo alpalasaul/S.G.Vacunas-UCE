8ee2f420398156294c6882a19cf1d5f1e0ae0993
Erick E. Diaz P <erickdp@hotmail.com>
2021-08-11 18:48:50 -0500

    Inicio de Proyecto

---
+++ README.md
@@ -0,0 +1,12 @@
+Repositorio del proyecto de DSI - SGVacunas
+
+- Ramas
+ * saul
+ * erick
+
+- Endpoint en Postman
+ *  -- En Despliegue --
+
+- Dominio: HEROKU
+ *  -- En Despliegue --
+
---
+++ src/main/java/uce/proyect/SgVacunasSiApplication.java
@@ -0,0 +1,13 @@
+package uce.proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgVacunasSiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgVacunasSiApplication.class, args);
+    }
+
+}
