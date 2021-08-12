3aa8676a94c6ae4c1a71ea2ec2e4faf2216ffa21
Erick E. Diaz P <erickdp@hotmail.com>
2021-08-11 20:38:46 -0500

    Deploy Heroku & Conexion a mlab

--- src/main/java/uce/proyect/models/User.java
+++ src/main/java/uce/proyect/models/User.java
@@ -1,6 +1,7 @@
 package uce.proyect.models;
 
 import lombok.Data;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.persistence.Id;
@@ -10,6 +11,7 @@ import javax.persistence.Id;
 public class User {
     @Id
     private String id;
+    @Indexed(unique = true)
     private String nombreUsuario;
     private String contrasena;
 }
