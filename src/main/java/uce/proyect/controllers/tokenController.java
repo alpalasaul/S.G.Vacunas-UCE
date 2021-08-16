e37309207bb489a707dc32e13be91ad5d0b2db61
Erick E. Diaz P <erickdp@hotmail.com>
2021-08-16 18:27:42 -0500

    Todos los puertos

--- src/main/java/uce/proyect/controllers/tokenController.java
+++ src/main/java/uce/proyect/controllers/tokenController.java
@@ -8,6 +8,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/sgv")
+@CrossOrigin(origins = "*")
 public class tokenController {
 
     @Autowired
