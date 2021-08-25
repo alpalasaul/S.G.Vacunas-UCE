f5fe03e47e98bb45df7614675fabde334a42b030
Erick E. Diaz P <erickdp@hotmail.com>
2021-08-24 22:11:29 -0500

    Ocultar detalles de estudiante

--- src/main/java/uce/proyect/models/Estudiante.java
+++ src/main/java/uce/proyect/models/Estudiante.java
@@ -1,11 +1,14 @@
 package uce.proyect.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document
 public class Estudiante extends Persona {
+    @JsonIgnore
     private int semestre;
+    @JsonIgnore
     private String facultad; // Identificador del objeto facultad para obtener la carrera
 }
