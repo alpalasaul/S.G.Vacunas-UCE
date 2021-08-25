572e2aca87b16f4144da9dada537cfc651283d15
Erick E. Diaz P <erickdp@hotmail.com>
2021-08-25 12:44:41 -0500

    Fixed Log Estudiante 1.2

--- src/main/java/uce/proyect/controllers/estudianteController.java
+++ src/main/java/uce/proyect/controllers/estudianteController.java
@@ -61,7 +61,7 @@ public class estudianteController {
         log.info(user.getApellidos());
         log.info(user.getCorreo());
         log.info(user.getCarrera());
-        log.info(user.getSemestre());
+        log.info(String.valueOf(user.getSemestre()));
         var nUser = this.estudianteService.agregar(user);
         this.emailService.enviarEmailCredenciales(
                 user.getCorreo(),
